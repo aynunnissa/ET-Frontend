f108e65f6909f42f1b41b717b54b52aabb4f4892
Aynun Nissa Setiawan <aynun@GTJKTMAC-159.local>
2024-10-17 23:18:08 +0700

    feat: add layout component

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,27 @@ import './App.css';
 import Header from './components/layout/Header';
 import Button from './components/shared/Button';
 import ChevronLeft from "./assets/icons/chevron-left.svg";
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <header className="App-header bg-green-600">
-        <h1 className='text-3xl font-bold underline'>Hello again, world!</h1>
-        <p className="title-1 md:title-lg">This is title 1</p>
-        <Button 
-          text='Mitra Petani'
-        />
-        <Button 
-          isIconBtn={true}
-          isRounded={true}
-          icon={ChevronLeft}
-          altText=''
-        />
-      </header>
+      <Layout>
+        <header className="App-header bg-green-600">
+          <h1 className='text-3xl font-bold underline'>Hello again, world!</h1>
+          <p className="title-1 md:title-lg">This is title 1</p>
+          <Button 
+            text='Mitra Petani'
+          />
+          <Button 
+            isIconBtn={true}
+            isRounded={true}
+            icon={ChevronLeft}
+            altText=''
+          />
+        </header>
+      </Layout>
+      
     </div>
   );
 }
---
+++ src/components/layout/Layout/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react"
+import Header from "../Header"
+
+interface IProps {
+  children: ReactNode
+}
+
+const Layout = ({ children }: IProps) => {
+  return(
+    <>
+      <Header />
+      {children}
+    </>
+  )
+}
+
+export default Layout;
