9c19cdd6b86ffda1e4c7835cd56358947db56202
Aynun Nissa Setiawan <aynun@GTJKTMAC-159.local>
2024-10-18 14:39:34 +0700

    feat: add use input hook

---
+++ src/hooks/use-input.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+const useInput = (validateValue: Function) => {
+  const [enteredValue, setEnteredValue] = useState<string>('');
+  const [isTouched, setIsTouched] = useState<boolean>(false);
+
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const inputBlurHandler = (event: React.SyntheticEvent): void => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue('');
+    setIsTouched(false);
+  };
+
+  return {
+    value: enteredValue,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
+
+export default useInput;
