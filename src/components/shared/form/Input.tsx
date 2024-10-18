aa4e7cdf341d3807c3c0f437de0a253461e6a930
Aynun Nissa Setiawan <aynun@GTJKTMAC-159.local>
2024-10-18 14:39:58 +0700

    feat: add reusable form components

---
+++ src/components/shared/form/Input.tsx
@@ -0,0 +1,33 @@
+import { FORM_ERROR } from "../../contact-us/constants";
+import styles from "./form.module.css";
+
+interface IProps {
+  type: string,
+  placeholder: string,
+  value: string,
+  class: string,
+  hasError: boolean,
+  valueChangeHandler: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  inputBlurHandler: (event: React.SyntheticEvent) => void,
+}
+
+const Input = (props: IProps) => {
+  return(
+    <div className={props.class}>
+      <input
+        type="text"
+        id={props.type}
+        className={styles.form__input}
+        onChange={props.valueChangeHandler}
+        onBlur={props.inputBlurHandler}
+        value={props.value}
+        placeholder={props.placeholder}
+      />
+      {props.hasError && (
+        <p className="text-et-red-main text-left mt-1 ml-0.5">{FORM_ERROR[props.type]}</p>
+      )}
+    </div>
+  )
+}
+
+export default Input;
